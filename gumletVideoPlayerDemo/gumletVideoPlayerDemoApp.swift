552076d931b8dd3fa053965de94c79ee49f88b9c
Prashant Pandey <prashant.krishna52@gmail.com>
2026-01-30 20:15:20 +0530

    Initial Commit

---
+++ gumletVideoPlayerDemo/gumletVideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gumletVideoPlayerDemoApp.swift
+//  gumletVideoPlayerDemo
+//
+//  Created by Prashant Pandey on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct gumletVideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
